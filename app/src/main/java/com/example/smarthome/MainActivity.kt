eca79257feee1e737b7032e68d5c9a3c584f3e30
C-Parindya <parindya6@gmail.com>
2026-07-12 21:45:32 +0530

    login screen

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -3,45 +3,167 @@ package com.example.smarthome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.smarthome.ui.theme.SmartHomeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            SmartHomeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartHomeTheme {
-        Greeting("Android")
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var visible by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        // Background Image
+        Image(
+            painter = painterResource(id = R.drawable.house),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(400.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(),
+
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White.copy(alpha = 0.60f)
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 10.dp
+                )
+            ) {
+                Column(
+                    modifier = Modifier.padding(25.dp)
+                ) {
+
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = { name = it },
+                        label = { Text("Username") },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+
+                            unfocusedBorderColor = Color.White.copy(alpha = 0.8f),
+
+                            focusedBorderColor = Color(0xFFB8865B),
+
+                            unfocusedContainerColor = Color.White.copy(alpha = 0.5f),
+
+                            focusedContainerColor = Color.White.copy(alpha = 0.8f)
+
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = OutlinedTextFieldDefaults.colors(
+
+                            unfocusedBorderColor = Color.White.copy(alpha = 0.8f),
+
+                            focusedBorderColor = Color(0xFFB8865B),
+
+                            unfocusedContainerColor = Color.White.copy(alpha = 0.5f),
+
+                            focusedContainerColor = Color.White.copy(alpha = 0.8f)
+
+                        ),
+                        shape = RoundedCornerShape(10.dp),
+                        visualTransformation =
+                            if (visible)
+                                VisualTransformation.None
+                            else
+                                PasswordVisualTransformation(),
+                        trailingIcon = {
+                            IconButton(onClick = {
+                                visible = !visible
+                            }) {
+                                Icon(
+                                    imageVector =
+                                        if (visible)
+                                            Icons.Default.Visibility
+                                        else
+                                            Icons.Default.VisibilityOff,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(
+                        text = "Forgot Password?",
+                        color = Color(0xFFA97A50),
+                        modifier = Modifier.align(Alignment.End)
+                    )
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(55.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFA97A50)   // Light brown
+                        )
+                    ) {
+                        Text("Login", fontSize = 18.sp)
+                    }
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        text = "Don't have an account? Sign Up",
+                        color = Color(0xFFA97A50)
+                    )
+                }
+            }
+        }
     }
 }
